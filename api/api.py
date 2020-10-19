3a8e8e60748c805b983acfc0b8b1224c33e1c670
Keifer Convertino <kjc6@rice.edu>
2020-10-19 00:06:31 -0500

    lottery simulation added

--- api/api.py
+++ api/api.py
@@ -1,11 +1,20 @@
 from flask import Flask, g
 import sqlite3
-import time
+from flask.json import jsonify
 import os
-import sys
+import random
+
 app = Flask(__name__)
 
 DATABASE = 'nba.db'
+
+lottery_balls = [140, 140, 140, 125, 105, 90, 75, 60, 45, 30, 20, 15, 10, 5]
+lottery_odds = [0 for i in range(len(lottery_balls))]
+lottery_odds[0] = lottery_balls[0]
+
+for i in range(1,len(lottery_balls)):
+    lottery_odds[i] = lottery_odds[i-1] + lottery_balls[i]
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -35,8 +44,45 @@ def query_db(query, args=()):
     return rv
 
 
-@app.route('/time')
-def get_current_time():
-    res = query_db('SELECT PLAYER_NAME FROM PLAYER_STATS ORDER BY RANDOM() LIMIT 1')
-    return res[0]
-    return {'time': time.time()}
+@app.route('/test')
+def get_test():
+    res = query_db('SELECT PLAYER_NAME, PTS, GP FROM PLAYER_STATS ORDER BY RANDOM() LIMIT 1')
+    return jsonify(res)
+
+@app.route('/playoff_teams')
+def get_teams():
+    query = 'SELECT TeamCity, WinPCT, Record FROM TEAM_STATS WHERE PlayoffRank <= 8 ORDER BY WinPCT'
+    res = query_db(query)
+    return jsonify(res)
+
+@app.route('/lottery_teams')
+def get_lottery_teams():
+    query = 'SELECT TeamCity, WinPCT, Record FROM TEAM_STATS WHERE PlayoffRank > 8 ORDER BY WinPCT'
+    res = query_db(query)
+    return jsonify(res)
+
+@app.route('/simulate')
+def simulate_lottery():
+    query = 'SELECT TeamCity, WinPCT, Record FROM TEAM_STATS WHERE PlayoffRank > 8 ORDER BY WinPCT'
+    lottery_teams = query_db(query)
+    rank = {}
+
+    for i, team in enumerate(lottery_teams):
+        rank[i] = team
+
+    picks_made = []
+    while len(picks_made) < 4:
+        ball = random.randint(0,1000)
+        for i, odds in enumerate(lottery_odds):
+            if odds > ball:
+                if i not in picks_made:
+                    picks_made.append(i)
+                break
+
+    res = [rank[pick] for pick in picks_made]
+
+    for i in range(len(lottery_teams)):
+        if i not in picks_made:
+            res.append(rank[i])
+
+    return jsonify(res)
--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTable } from 'react-table'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [currentPlayer, setCurrentPlayer] = useState("");
+  const [currentGP, setCurrentGP] = useState(1);
+  const [currentPTS, setCurrentPTS] = useState(0);
 
+  const [lotteryTeams, setLotteryTeams] = useState([])
+  const [playoffTeams, setPlayoffTeams] = useState([])
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      console.log(data)
-      setCurrentTime(data.PLAYER_NAME)
+    fetch('/test').then(res => res.json()).then(data => {
+      console.log(data[0])
+      let player = data[0]
+      setCurrentPlayer(player.PLAYER_NAME)
+      setCurrentGP(player.GP)
+      setCurrentPTS(player.PTS)
     });
   }, [])
+
+  useEffect(() => {
+    fetch('/playoff_teams').then(res => res.json()).then(data => {
+      console.log(data)
+      setPlayoffTeams(data)
+    });
+  }, [])
+
+  useEffect(() => {
+    fetch('/lottery_teams').then(res => res.json()).then(data => {
+      console.log(data)
+      setLotteryTeams(data)
+    });
+  }, [])
+
+  function simulateLottery() {
+    fetch('/simulate').then(res => res.json()).then(data => {
+      console.log(data)
+      setLotteryTeams(data)
+    });
+  }
+
+  function reset() {
+    fetch('/lottery_teams').then(res => res.json()).then(data => {
+      console.log(data)
+      setLotteryTeams(data)
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -26,7 +62,26 @@ function App() {
         >
           Learn React
         </a>
-        <p>Random Player is {currentTime}.</p>
+        <div>
+          <button className="simulate" onClick={simulateLottery}>Simulate</button>
+          <button className="button" onClick={reset}>Reset</button>
+        </div>
+        
+        <tbody>
+          {lotteryTeams.map(team => <tr key={team.TeamCity}>
+            <td className='city'>{team.TeamCity}</td>
+            <td className='win-pct'>{team.WinPCT}</td>
+            <td className='record'>{team.Record}</td>
+          </tr>)}
+          {playoffTeams.map(team => <tr key={team.TeamCity}>
+            <td className='city'>{team.TeamCity}</td>
+            <td className='win-pct'>{team.WinPCT}</td>
+            <td className='record'>{team.Record}</td>
+          </tr>)}
+
+          
+        </tbody>
+        <p>{currentPlayer} averages {(currentPTS / currentGP).toFixed(2)} points per game.</p>
       </header>
     </div>
   );
